b53be559db1968e54a260424c9e66adafb5ea0b2
GJSK <Tim_GJSK@hotmail.com>
2023-05-21 19:27:59 -0400

    Fixed asynchronous Rx bug in cv interface

--- application/cv_usart_task.h
+++ application/cv_usart_task.h
@@ -26,10 +26,10 @@ typedef struct __attribute__((packed))
 
 typedef enum
 {
-    EDGE_NONE,
-    EDGE_RISING,
-    EDGE_FALLING,
-} eEdgeTypes;
+    CV_MODE_AUTO_AIM_BIT = 1 << 0,
+    CV_MODE_AUTO_MOVE_BIT = 1 << 1,
+    CV_MODE_ENEMY_DETECTED_BIT = 1 << 2,
+} eModeControlBits;
 
 /**
  * @brief main handler of communication status and commands received from CV
@@ -37,10 +37,10 @@ typedef enum
 typedef struct
 {
     tCvCmdMsg CvCmdMsg;
-    uint8_t fCvCmdValid; ///< whether CvCmdMsg is valid as received from CV; to be used by gimbal_task
+    uint8_t fCvCmdValid; ///< to be used by gimbal_task, but not chassis_task. chassis_task should maintain previous speed if cv is offline for a short time
     uint8_t fIsWaitingForAck;
     uint8_t fCvMode; ///< contains individual CV control flag bits defined by eModeControlBits
-    eEdgeTypes fIsAutoAimSwitchEdge;
+    uint8_t fIsModeChanged;
     const RC_ctrl_t *cv_rc_ctrl; ///< remote control pointer
 } tCvCmdHandler;
 
@@ -51,6 +51,8 @@ void CvCmder_DetectAutoAimSwitchEdge(uint8_t fRcCmd);
 #if defined(DEBUG_CV)
 uint8_t CvCmder_CheckAndResetUserKeyEdge(void);
 #endif // defined(DEBUG_CV)
+
+extern tCvCmdHandler CvCmdHandler;
 
 #endif // defined(CV_INTERFACE)
 
