a5c845c1feb76eca5d652ded339b808b506c578f
GJSK-Novice <Tim_GJSK@hotmail.com>
2023-07-01 21:37:09 -0400

    Adjusted friction motor speed

--- bsp/boards/bsp_fric.h
+++ bsp/boards/bsp_fric.h
@@ -2,19 +2,24 @@
 #define BSP_FRIC_H
 #include "struct_typedef.h"
 
-#if defined(INFANTRY_2)
+#if defined(INFANTRY_1)
+#define FRIC_UP 1300
+#define FRIC_DOWN 1320
+#elif defined(INFANTRY_2)
 // M3508 as friction wheel motor: 1080us-1920us pulse width range corresponds to 0-v_max rpm
 // rpm = (Width-1080)/(1920-1080)*v_max; where v_max = 8000rpm by default, Width= 1000000s/us * CCR/ARR / 50Hz = 1000000*CCR/20000/50 = CCR
-// Also, rpm = v/radius*60/2pi; where radius = 0.03m, thus, rpm = 1000/pi*v
+// Also, rpm = v/radius*60/2pi; where radius = 0.03m, thus, rpm = 1000/pi*vw
 // After simplification, CCR=105/pi*v+1080, or v=pi*(CCR-1080)/105
-#define FRIC_UP 1920 // 4456rpm, 14m/s
-// #define FRIC_UP 1615 // 5093rpm, 16m/s
+// Test result after linear regression: CCR = 20.45452*v + 1167.47328
+#define FRIC_UP 1470 // test result: fluctuate between 14.0m/s and 14.7m/s
 #define FRIC_DOWN 1648
-#elif defined(INFANTRY_1) || defined(SENTRY_1)
-#define FRIC_UP 1400
-#define FRIC_DOWN 1320
 #elif defined(INFANTRY_3)
-#define FRIC_UP 1300 // test result on INFANTRY_3 (M2305 snail motor): slightly below 15m/s
+#define FRIC_UP 1310 // test result on INFANTRY_3 (M2305 snail motor): slightly below 15m/s
+#define FRIC_DOWN 1320
+#elif defined(SENTRY_1)
+// sentry limit by rule: 28m/s
+// test result: 24.5m/s for safety
+#define FRIC_UP 1500
 #define FRIC_DOWN 1320
 #endif
 #define FRIC_OFF 1000
