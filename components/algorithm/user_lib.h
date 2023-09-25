4b18acd2f94aea78b43998c52ffa038dc5a870f2
GJSK-Novice <Tim_GJSK@hotmail.com>
2023-09-25 17:05:20 -0400

    Added STATIC_ASSERT to library

--- components/algorithm/user_lib.h
+++ components/algorithm/user_lib.h
@@ -2,6 +2,34 @@
 #define USER_LIB_H
 #include "struct_typedef.h"
 
+/** A compile time assertion check.
+ *
+ *  Validate at compile time that the predicate is true without
+ *  generating code. This can be used at any point in a source file
+ *  where typedef is legal.
+ *
+ *  On success, compilation proceeds normally.
+ *
+ *  On failure, attempts to typedef an array type of negative size. The
+ *  offending line will look like
+ *      typedef assertion_failed_file_h_42[-1]
+ *  where file is the content of the second parameter which should
+ *  typically be related in some obvious way to the containing file
+ *  name, 42 is the line number in the file on which the assertion
+ *  appears, and -1 is the result of a calculation based on the
+ *  predicate failing.
+ *
+ *  \param predicate The predicate to test. It must evaluate to
+ *  something that can be coerced to a normal C boolean.
+ *
+ *  \param file A sequence of legal identifier characters that should
+ *  uniquely identify the source file in which this condition appears.
+ */
+#define STATIC_ASSERT(predicate) _impl_CASSERT_LINE(predicate,__LINE__,__FILE__)
+#define _impl_PASTE(a,b) a##b
+#define _impl_CASSERT_LINE(predicate, line, file) \
+    typedef char _impl_PASTE(assertion_failed_##file##_,line)[2*!!(predicate)-1];
+
 #define HALF_ECD_RANGE 4096
 #define ECD_RANGE 8192
 #define MOTOR_RAD_TO_ECD 1303.7972938088067f  // 8192/(2*PI)
