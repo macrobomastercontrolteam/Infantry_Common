af2efe11f30021c171565f4c255a56081e9be870
GJSK-Novice <Tim_GJSK@hotmail.com>
2024-05-04 23:13:23 -0400

    Expanded trigger motor ratios

--- application/shoot.h
+++ application/shoot.h
@@ -50,8 +50,7 @@
 //摩擦轮高速 加速 时间
 #define UP_ADD_TIME                 80
 //拨弹电机rmp 变化成 旋转速度的比例
-#define TRIGGER_MOTOR_RPM_TO_SPEED  0.00290888208665721596153948461415f
-#define TRIGGER_MOTOR_ECD_TO_ANGLE  0.000021305288720633905968306772076277f
+#define TRIGGER_MOTOR_GEAR_RATIO  36.0f
 #define FULL_COUNT                  18
 //拨弹速度
 #define TRIGGER_SPEED               10.0f
