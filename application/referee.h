fae2a15c8af9d77d49910b79ae5b4f12d5134d10
GJSK-Novice <Tim_GJSK@hotmail.com>
2024-05-18 23:01:03 -0400

    Fixed ref heat API

--- application/referee.h
+++ application/referee.h
@@ -184,6 +184,5 @@ extern void get_chassis_power_and_buffer(fp32 *power, fp32 *buffer);
 
 extern uint8_t get_robot_id(void);
 
-extern void get_shoot_heat0_limit_and_heat0(uint16_t *heat0_limit, uint16_t *heat0);
-extern void get_shoot_heat1_limit_and_heat1(uint16_t *heat1_limit, uint16_t *heat1);
+extern void get_shoot_heat_limit_and_heat(uint16_t *heat0_limit, uint16_t *heat0);
 #endif
