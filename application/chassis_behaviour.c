e2bf470e388e5167c19d9fc2b0b6e6b15f676eb3
GJSK-Novice <Tim_GJSK@hotmail.com>
2024-06-06 04:25:01 -0400

    Minor fix

--- application/chassis_behaviour.c
+++ application/chassis_behaviour.c
@@ -180,7 +180,7 @@ void chassis_behaviour_mode_set(chassis_move_t *chassis_move_mode)
 #if (ROBOT_TYPE == SENTRY_2023_MECANUM)
 				chassis_behaviour_mode = CHASSIS_CV_CONTROL_SPINNING;
 #else
-				chassis_behaviour_mode = CHASSIS_NO_FOLLOW_YAW;
+				chassis_behaviour_mode = CHASSIS_SPINNING;
 #endif
 				break;
 			}
