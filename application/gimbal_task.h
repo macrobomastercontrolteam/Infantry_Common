e56e81196cf1c5aa680e7d6ecc0e1a717df83c79
MichaelZhang830 <michaelzhang732@gmail.com>
2024-07-18 09:00:43 -0400

    Added gimbal abs angles to the cv uart payload

--- application/gimbal_task.h
+++ application/gimbal_task.h
@@ -368,4 +368,7 @@ extern fp32 motor_ecd_to_angle_change(uint16_t ecd, uint16_t offset_ecd);
 
 extern bool_t gimbal_emergency_stop(void);
 extern gimbal_control_t gimbal_control;
+
+fp32 get_gimbal_yaw_angle(void);
+fp32 get_gimbal_pitch_angle(void);
 #endif
