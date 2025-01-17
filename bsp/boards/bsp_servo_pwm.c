ff9e9e6a109da9fb15e363bfba308dbcb8d964eb
Ting Meng <mengt7@mcmaster.ca>
2025-01-17 11:58:40 -0500

    Added Servo Control

--- bsp/boards/bsp_servo_pwm.c
+++ bsp/boards/bsp_servo_pwm.c
@@ -1,28 +1,31 @@
 #include "bsp_servo_pwm.h"
 #include "main.h"
 
+#define SERVO_MIN_PWM 500
+#define SERVO_MAX_PWM 2500
 extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim8;
 
-void servo_pwm_set(uint16_t pwm, uint8_t i)
+void servo_pwm_set(uint16_t pwm)
 {
-    switch(i)
+    uint16_t set_pwm = pwm;
+    if (set_pwm < SERVO_MIN_PWM)
     {
-        case 0:
-        {
-            __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_2, pwm);
-        }break;
-        case 1:
-        {
-            __HAL_TIM_SetCompare(&htim8, TIM_CHANNEL_1, pwm);
-        }break;
-        case 2:
-        {
-            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_4, pwm);
-        }break;
-        case 3:
-        {
-            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_3, pwm);
-        }break;
+        set_pwm = SERVO_MIN_PWM;
     }
+    else if (set_pwm > SERVO_MAX_PWM)
+    {
+        set_pwm = SERVO_MAX_PWM;
+    }
+    __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, set_pwm);
+}
+
+void servo_open()
+{
+    servo_pwm_set(2100);
+}
+
+void servo_close()
+{
+    servo_pwm_set(1300);
 }
--- bsp/boards/bsp_servo_pwm.h
+++ bsp/boards/bsp_servo_pwm.h
@@ -2,6 +2,7 @@
 #define BSP_SERVO_PWM_H
 #include "global_inc.h"
 
-extern void servo_pwm_set(uint16_t pwm, uint8_t i);
-
+extern void servo_pwm_set(uint16_t pwm);
+extern void servo_open(void);
+extern void servo_close(void);
 #endif
