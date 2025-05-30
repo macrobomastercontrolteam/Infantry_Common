edbcd5e882e9132a1759a4c2a95decf0529ff8aa
Ting Meng <mengt7@mcmaster.ca>
2025-05-29 20:53:59 -0400

    fixed typo in CAN_ref, updated refree code base on protocol 1.7.0

--- application/referee.c
+++ application/referee.c
@@ -21,7 +21,7 @@ ext_game_robot_HP_t game_robot_HP_t; // 0x0003
 // ext_ICRA_buff_debuff_zone_and_lurk_status_t ICRA_buff_debuff_zone_and_lurk_status_t; //0x0005
 
 ext_event_data_t field_event;                              // 0x0101
-ext_supply_projectile_action_t supply_projectile_action_t; // 0x0102
+//ext_supply_projectile_action_t supply_projectile_action_t; // 0x0102
 // ext_supply_projectile_booking_t supply_projectile_booking_t; //0x0103
 ext_referee_warning_t referee_warning_t; // 0x0104
 
@@ -29,7 +29,7 @@ ext_game_robot_state_t robot_state;                // 0x0201
 ext_power_heat_data_t power_heat_data_t;           // 0x0202
 ext_game_robot_pos_t game_robot_pos_t;             // 0x0203
 ext_buff_musk_t buff_musk_t;                       // 0x0204
-ext_aerial_robot_energy_t robot_energy_t;          // 0x0205
+//ext_aerial_robot_energy_t robot_energy_t;          // 0x0205
 ext_robot_hurt_t robot_hurt_t;                     // 0x0206
 ext_shoot_data_t shoot_data_t;                     // 0x0207
 ext_rfid_status_t rfid_status_t;                   // 0x0208
@@ -70,7 +70,7 @@ void init_referee_struct_data(void)
 	memset(&game_robot_HP_t, 0, sizeof(ext_game_robot_HP_t));
 
 	memset(&field_event, 0, sizeof(ext_event_data_t));
-	memset(&supply_projectile_action_t, 0, sizeof(ext_supply_projectile_action_t));
+	//memset(&supply_projectile_action_t, 0, sizeof(ext_supply_projectile_action_t));
 	// memset(&supply_projectile_booking_t, 0, sizeof(ext_supply_projectile_booking_t));
 	memset(&referee_warning_t, 0, sizeof(ext_referee_warning_t));
 
@@ -151,11 +151,11 @@ void referee_data_solve(uint8_t *frame)
 			memcpy(&field_event, frame + index, sizeof(field_event));
 			break;
 		}
-		case SUPPLY_PROJECTILE_ACTION_CMD_ID:
-		{
-			memcpy(&supply_projectile_action_t, frame + index, sizeof(supply_projectile_action_t));
-			break;
-		}
+		// case SUPPLY_PROJECTILE_ACTION_CMD_ID:
+		// {
+		// 	memcpy(&supply_projectile_action_t, frame + index, sizeof(supply_projectile_action_t));
+		// 	break;
+		// }
 		// case SUPPLY_PROJECTILE_BOOKING_CMD_ID:
 		// {
 		//     memcpy(&supply_projectile_booking_t, frame + index, sizeof(supply_projectile_booking_t));
@@ -276,11 +276,11 @@ void referee_data_solve(uint8_t *frame)
 		//     memcpy(&dart_client_cmd_t, frame + index, sizeof(dart_client_cmd_t));
 		//     break;
 		// }
-		case STUDENT_INTERACTIVE_DATA_CMD_ID:
-		{
-			memcpy(&student_interactive_data_t, frame + index, sizeof(student_interactive_data_t));
-			break;
-		}
+		// case STUDENT_INTERACTIVE_DATA_CMD_ID:
+		// {
+		// 	memcpy(&student_interactive_data_t, frame + index, sizeof(student_interactive_data_t));
+		// 	break;
+		// }
 		// Specific for engineer robot
 		// case DIY_controller_DATA_SEND_ID:
         // {
@@ -316,9 +316,8 @@ void referee_data_solve(uint8_t *frame)
 	}
 }
 
-void get_chassis_power_data(fp32 *power, fp32 *buffer, fp32 *power_limit)
+void get_chassis_power_data(fp32 *buffer, fp32 *power_limit)
 {
-	*power = power_heat_data_t.chassis_power;
 	*buffer = power_heat_data_t.buffer_energy;
 	if (robot_state.chassis_power_limit > 0)
 	{
@@ -365,6 +364,7 @@ uint8_t get_team_color(void)
 			break;
 		}
 	}
+	
 	return team_color;
 }
 
--- application/referee.h
+++ application/referee.h
@@ -7,7 +7,7 @@
 
 #include "protocol.h"
 // #include "user_lib.h"
-
+#define MAX_USER_DATA_LEN 112
 typedef enum
 {
 	RED_HERO = 1,
@@ -17,13 +17,13 @@ typedef enum
 	RED_STANDARD_3 = 5,
 	RED_AERIAL = 6,
 	RED_SENTRY = 7,
-	BLUE_HERO = 11,
-	BLUE_ENGINEER = 12,
-	BLUE_STANDARD_1 = 13,
-	BLUE_STANDARD_2 = 14,
-	BLUE_STANDARD_3 = 15,
-	BLUE_AERIAL = 16,
-	BLUE_SENTRY = 17,
+	BLUE_HERO = 101,
+	BLUE_ENGINEER = 102,
+	BLUE_STANDARD_1 = 103,
+	BLUE_STANDARD_2 = 104,
+	BLUE_STANDARD_3 = 105,
+	BLUE_AERIAL = 106,
+	BLUE_SENTRY = 107,
 } robot_id_t;
 typedef enum
 {
@@ -62,7 +62,7 @@ typedef __packed struct // 0003
 	uint16_t red_2_robot_HP;
 	uint16_t red_3_robot_HP;
 	uint16_t red_4_robot_HP;
-	uint16_t red_5_robot_HP;
+	uint16_t red_5_robot_HP; //reserved
 	uint16_t red_7_robot_HP;
 	uint16_t red_outpost_HP;
 	uint16_t red_base_HP;
@@ -70,7 +70,7 @@ typedef __packed struct // 0003
 	uint16_t blue_2_robot_HP;
 	uint16_t blue_3_robot_HP;
 	uint16_t blue_4_robot_HP;
-	uint16_t blue_5_robot_HP;
+	uint16_t blue_5_robot_HP; //reserved
 	uint16_t blue_7_robot_HP;
 	uint16_t blue_outpost_HP;
 	uint16_t blue_base_HP;
@@ -103,13 +103,13 @@ typedef __packed struct // 0x0101
 	uint32_t event_data;
 } ext_event_data_t;
 
-typedef __packed struct // 0x0102
-{
-	uint8_t supply_projectile_id;
-	uint8_t supply_robot_id;
-	uint8_t supply_projectile_step;
-	uint8_t supply_projectile_num;
-} ext_supply_projectile_action_t;
+// typedef __packed struct // 0x0102
+// {
+// 	uint8_t supply_projectile_id;
+// 	uint8_t supply_robot_id;
+// 	uint8_t supply_projectile_step;
+// 	uint8_t supply_projectile_num;
+// } ext_supply_projectile_action_t;
 
 // typedef __packed struct // 0x0103
 // {
@@ -130,11 +130,12 @@ typedef __packed struct // 0x0104
 	uint8_t count;
 } ext_referee_warning_t;
 
-// typedef __packed struct //0x0105
-// {
-//  uint8_t dart_remaining_time;
-//  uint8_t dart_aim_state;
-// }dart_info_t;
+typedef __packed struct //0x0105
+{
+ uint8_t dart_remaining_time;
+ //uint8_t dart_aim_state;
+ uint16_t dart_info; 
+}dart_info_t;
 
 typedef __packed struct // 0x0201
 {
@@ -154,7 +155,7 @@ typedef __packed struct // 0x0202
 {
 	uint16_t chassis_voltage;
 	uint16_t chassis_current;
-	float chassis_power;
+	float chassis_power; 			//reserved
 	uint16_t buffer_energy;
 	uint16_t shooter_17mm_1_barrel_heat;
 	uint16_t shooter_17mm_2_barrel_heat;
@@ -173,14 +174,16 @@ typedef __packed struct // 0x0204
 	uint8_t recovery_buff;
 	uint8_t cooling_buff;
 	uint8_t defence_buff;
+	uint8_t vulnerability_buff; 
 	uint16_t attack_buff;
+	uint8_t remaining_energy; 
 } ext_buff_musk_t;
 
-typedef __packed struct // 0x0205
-{
-	uint8_t airforce_status;
-	uint8_t time_remain;
-} ext_aerial_robot_energy_t;
+// typedef __packed struct // 0x0205
+// {
+// 	uint8_t airforce_status;
+// 	uint8_t time_remain;
+// } ext_aerial_robot_energy_t;
 
 typedef __packed struct // 0x0206
 {
@@ -210,17 +213,17 @@ typedef __packed struct // 0x0209
 	uint32_t rfid_status;
 } ext_rfid_status_t;
 
-// typedef __packed struct // 0x020A
-// {
-//      uint8_t dart_launch_opening_status;
-//      uint8_t reserved;
-//      uint16_t target_change_time;
-//      uint16_t latest_launch_cmd_time;
-//     uint8_t dart_launch_opening_status;
-//     uint8_t dart_attack_target;
-//     uint16_t target_change_time;
-//     uint16_t latest_launch_cmd_time;
-// } ext_dart_client_cmd_t;
+typedef __packed struct // 0x020A
+{
+    uint8_t dart_launch_opening_status;
+    uint8_t reserved;
+    uint16_t target_change_time;
+    uint16_t latest_launch_cmd_time;
+    // uint8_t dart_launch_opening_status;
+    // uint8_t dart_attack_target;
+    // uint16_t target_change_time;
+    // uint16_t latest_launch_cmd_time;
+} ext_dart_client_cmd_t;
 
 typedef __packed struct // 0x020B
 {
@@ -232,23 +235,25 @@ typedef __packed struct // 0x020B
 	float standard_3_y;
 	float standard_4_x;
 	float standard_4_y;
-	float standard_5_x;
-	float standard_5_y;
+	float standard_5_x;//reserved
+	float standard_5_y;//reserved
 } ext_ground_robot_position_t;
 
 typedef __packed struct // 0x020C
 {
-	uint8_t mark_hero_progress;
-	uint8_t mark_engineer_progress;
-	uint8_t mark_standard_3_progress;
-	uint8_t mark_standard_4_progress;
-	uint8_t mark_standard_5_progress;
-	uint8_t mark_sentry_progress;
+	uint8_t mark_progress;
+	// uint8_t mark_hero_progress;
+	// uint8_t mark_engineer_progress;
+	// uint8_t mark_standard_3_progress;
+	// uint8_t mark_standard_4_progress;
+	// uint8_t mark_standard_5_progress;
+	// uint8_t mark_sentry_progress;
 } ext_radar_mark_data_t;
 
 typedef __packed struct // 0x020D
 {
 	uint32_t sentry_info;
+	uint16_t sentry_info_2;
 } ext_sentry_info_t;
 
 typedef __packed struct // 0x020E
@@ -256,10 +261,10 @@ typedef __packed struct // 0x020E
 	uint8_t radar_info;
 } ext_radar_info_t;
 
-typedef __packed struct
-{
-	uint8_t bullet_remaining_num;
-} ext_bullet_remaining_t;
+// typedef __packed struct// 0x020F
+// {
+// 	uint8_t bullet_remaining_num;
+// } ext_bullet_remaining_t;
 
 typedef __packed struct // 0x0301
 {
@@ -271,8 +276,41 @@ typedef __packed struct // 0x0301
 	uint16_t data_cmd_id;
 	uint16_t sender_id;
 	uint16_t receiver_id;
-	// uint8_t user_data[x]; //x <= 112
+	uint8_t user_data[MAX_USER_DATA_LEN]; //x <= 112
 } ext_student_interactive_data_t;
+typedef __packed struct // 0x100
+{
+	uint8_t delete_type;
+	uint8_t layer;
+} interaction_layer_delete_t;
+
+typedef __packed struct // 0x101
+{
+	uint8_t figure_name[3];
+	uint32_t operate_tpye : 3;
+	uint32_t figure_tpye : 3;
+	uint32_t layer : 4;
+	uint32_t color : 4;
+	uint32_t details_a : 9;
+	uint32_t details_b : 9;
+	uint32_t width : 10;
+	uint32_t start_x : 11;
+	uint32_t start_y : 11;
+	uint32_t details_c : 10;
+	uint32_t details_d : 11;
+	uint32_t details_e : 11;
+} interaction_figure_t;
+
+typedef __packed struct //0x102
+{ 
+  interaction_figure_t interaction_figure[2]; 
+
+}interaction_figure_2_t; 
+
+typedef __packed struct //0x103
+{ 
+interaction_figure_t interaction_figure[5]; 
+}interaction_figure_3_t; 
 
 // typedef __packed struct //0x0110
 // {
@@ -298,6 +336,7 @@ typedef __packed struct // 0x0121
 //     uint8_t target_robot_id;
 //     uint8_t cmd_source;
 // } ext_robot_command_t;
+
 //     uint8_t cmd_keyboard;
 //     uint8_t target_robot_id;
 //     uint8_t cmd_source;
@@ -345,8 +384,44 @@ typedef __packed struct // 0x0308
 {
 	uint16_t sender_id;
 	uint16_t receiver_id;
-	uint16_t user_data[30];
+	uint8_t user_data[30];
 } ext_custom_info_t;
+
+typedef __packed struct // 0x0303
+{
+	float target_position_x;
+	float target_position_y;
+	uint8_t cmd_keyboard;
+	uint8_t target_robot_id;
+	uint16_t cmd_source;
+} ext_map_command_t;
+
+typedef __packed struct // 0x0304
+{
+	int16_t mouse_x;
+	int16_t mouse_y;
+	int16_t mouse_z;
+	int8_t left_button_down;
+	int8_t right_button_down;
+	uint16_t keyboard_value;
+	uint16_t reserved;
+} ext_robot_keyboard_mouse_command_t;
+
+typedef __packed struct // 0x0305
+{
+	uint16_t hero_position_x;
+	uint16_t hero_position_y;
+	uint16_t engineer_position_x;
+	uint16_t engineer_position_y;
+	uint16_t infantry_3_position_x;
+	uint16_t infantry_3_position_y;
+	uint16_t infantry_4_position_x;
+	uint16_t infantry_4_position_y;
+	uint16_t infantry_5_position_x;
+	uint16_t infantry_5_position_y;
+	uint16_t sentry_position_x;
+	uint16_t sentry_position_y;
+} ext_map_robot_data_t;
 
 typedef __packed struct
 {
@@ -380,7 +455,7 @@ typedef __packed struct
 extern void init_referee_struct_data(void);
 extern void referee_data_solve(uint8_t *frame);
 
-extern void get_chassis_power_data(fp32 *power, fp32 *buffer, fp32 *power_limit);
+extern void get_chassis_power_data(fp32 *buffer, fp32 *power_limit);
 
 extern uint8_t get_robot_id(void);
 extern uint8_t get_team_color(void);
