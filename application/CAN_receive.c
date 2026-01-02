8c9c3c5f8f5ce86aad31c722682efa7294e76b31
MichaelZhang830 <michaelzhang732@gmail.com>
2026-01-01 20:57:37 -0500

    Changed: Default motor status to off

--- application/CAN_receive.c
+++ application/CAN_receive.c
@@ -36,9 +36,9 @@
 // Warning: because #if directive will assume the expression as 0 even if the macro is not defined, positive logic, for example, ENABLE_MOTOR_POWER, is safer that if and only if it's defined and set to 1 that the power is enabled
 
 //////////////enable for all robot types//////////////////////
-#define ENABLE_DRIVE_MOTOR_POWER 1
-#define ENABLE_YAW_MOTOR_POWER 1
-#define ENABLE_PITCH_MOTOR_POWER 1
+#define ENABLE_DRIVE_MOTOR_POWER 0
+#define ENABLE_YAW_MOTOR_POWER 0
+#define ENABLE_PITCH_MOTOR_POWER 0
 // Remember to enable ENABLE_SHOOT_REDUNDANT_SWITCH as well if you want to shoot
 #define ENABLE_TRIGGER_MOTOR_POWER 0
 #define ENABLE_FRICTION_1_MOTOR_POWER 0
